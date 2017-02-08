0aba04344f4588ea5cd3d14237e9b5ed5a5fd5b7
brucen <brucen83@gmail.com>
2017-02-08 09:13:38 +0200

    created write file func moved socket setup code from main to setup and run func

--- TCPFileSender.py
+++ TCPFileSender.py
@@ -1,6 +1,11 @@
 import socket
 import sys
 
+#file write
+def createFile():
+    with open('file_object.txt', 'w') as fileContent:
+        fileContent.write("no mas\n")
+        fileContent.close()
 
 #file read
 def readFile():
@@ -15,35 +20,52 @@ def readFile():
 def sendData(clientsock,addr):
     data = readFile()
     clientsock.send(bytes(data, 'UTF-8'))
-    
-if __name__=='__main__':
-    #create tcp socket
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     #forcibly bind to port in use
+
+def setupServer(address, port):
+    # create tcp socket
+    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # forcibly bind to port in use
     _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    #setup socket
-    server_address = ("", 4002)
-    socket.bind(server_address)
+    # setup socket
+    server_address = (address, port)
 
-    #listen for connections n=5
-    socket.listen(5)
+    _socket.bind(server_address)
 
-   while True:
-        #when client connections made, read from file and send to client socket
-        connection, client = _socket.accept()
-        
+    return _socket
+
+
+def runServer(sock):
+    # create that file here
+    createFile()
+    # listen for connections n=5
+    sock.listen(5)
+
+    while True:
+    # when client connections made, read from file and send to client socket
+        connection, client = sock.accept()
+        client_address = client
+
         try:
             print(sys.stderr, "waiting for connection")
-            #unfortunately keeps reading and sending the
-                #data but only to one client connection
-            #read and send data to               
+            # unfortunately keeps reading and sending the
+            # data but only to one client connection
+            # read and send data to
             if readFile():
-                #send data over socket
+                # send data over socket
                 sendData(connection, client)
             else:
                 print(sys.stderr, "sorry bru no more data")
-                break
         finally:
-            #clean up the connection
+            # clean up the connection
+            print(client)
             connection.close()
+
+if __name__=='__main__':
+
+    server_socket = setupServer("", 4006)
+
+    runServer(server_socket)
+
+
